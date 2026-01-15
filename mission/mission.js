8e972d0da0e53db2ac7af717e9eedb013240828b
jic91 <jicastro91@byui.edu>
2026-01-14 19:41:26 -0700

    Add theme selection functionality with dark mode support

---
+++ mission/mission.js
@@ -0,0 +1,20 @@
+const themeSelector = document.querySelector('#theme-selector');
+const body = document.querySelector('body');
+const logo = document.querySelector('img');
+
+function changeTheme() {
+    // (light or dark)
+    const currentTheme = themeSelector.value;
+
+    if (currentTheme === 'dark') {
+        
+        body.classList.add('dark');
+        // change logo
+        logo.setAttribute('src', 'byui-logo_white.png'); 
+    } else {
+        
+        body.classList.remove('dark');
+        // change logo 
+        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
+    }
+}
