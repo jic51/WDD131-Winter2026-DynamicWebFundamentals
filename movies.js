b03444bca1f1ccc0b4498ec51f6cf0b063a97e9b
jic91 <jicastro91@byui.edu>
2026-02-14 21:28:26 -0700

    Refactor HTML structure and CSS styles for improved layout and readability; update JavaScript to render book and movie reviews dynamically.

--- movies.js
+++ movies.js
@@ -43,24 +43,23 @@ const movies = [
     ];
 
     const movieList = document.querySelector('#movie-list');
-    movies.forEach(movie => {
-         const article = document.createElement('article');
-         article.className = 'movie';
-         
+        movies.forEach(movie => {
+            const article = document.createElement('article');
+            article.className = 'movie';
+            
 
-        let html = `
-                <h2>${movie.title}</h2>
-                <img src=${movie.imgSrc} alt=${movie.imgAlt}>
-                <p><strong>Release Date:</strong> ${movie.date}</p>
-                <p><strong>Recommended Age:</strong> ${movie.ages}</p>
-                <p><strong>Genre:</strong> ${movie.genre}</p>
-                <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
-                <p id='desc'>${movie.description}</p>
-              `
+            let html = `
+                    <h2>${movie.title}</h2>
+                    <img src=${movie.imgSrc} alt=${movie.imgAlt}>
+                    <p><strong>Release Date:</strong> ${movie.date}</p>
+                    <p><strong>Recommended Age:</strong> ${movie.ages}</p>
+                    <p><strong>Genre:</strong> ${movie.genre}</p>
+                    <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
+                    <p id='desc'>${movie.description}</p>
+                  `
 
-article.innerHTML += html;
-movieList.appendChild(article);
+    article.innerHTML += html;
+    movieList.appendChild(article);
 
-    });
-          
+        });
 
